16110bb2f8f8e50eeec92e35afb3e96c5fff91a9
LionelKarlen <lionel.karlen@protonmail.com>
2023-10-10 00:38:57 +0200

    feat: Add precommit hook

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.suspiciouslycool.stuff',
-  appName: 'stuff',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+	appId: 'org.suspiciouslycool.stuff',
+	appName: 'stuff',
+	webDir: 'dist',
+	server: {
+		androidScheme: 'https'
+	}
 };
 
 export default config;
